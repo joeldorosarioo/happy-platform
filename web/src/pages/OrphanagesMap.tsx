5a6ba2ce23fd3f7c0383f0eb68ec1d444e7c7871
Joel Oliveira <joeldorosarioo@gmail.com>
2020-10-12 22:03:15 -0300

    Creates the main page and location page components

---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import MapMarkerIcon from '../images/map-marker.svg';
+
+import '../stylesheets/pages/orphanages-map.css';
+
+function OrphanagesMap() {
+	return (
+		<div id="page-map">
+			<aside>
+				<header>
+					<img src={ MapMarkerIcon } alt="Happy" />
+
+					<h2>Escolha um orfanato no mapa</h2>
+					<p>Muitas crianças estão esperando a sua visita :)</p>
+				</header>
+
+				<footer>
+					<strong>São Paulo</strong>
+					<span>Embu das Artes</span>
+				</footer>
+			</aside>
+
+			<Map
+				center={[-23.6533217,-46.8497346]}
+				zoom={ 13 }
+				style={{ width: '100%', height: '100%' }}
+			>
+				<TileLayer
+					url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${ process.env.REACT_APP_MAPBOX_TOKEN }`}
+				/>
+			</Map>
+
+			<Link to="" className="create-orphanage">
+				<FiPlus size={ 32 } color="#FFFFFF" />
+			</Link>
+		</div>
+	);
+}
+
+export default OrphanagesMap;
